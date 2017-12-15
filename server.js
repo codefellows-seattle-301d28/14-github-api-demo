6763e39ca3cad8305099531cb30d0dc9c2615197
Allie <allie@codefellows.com>
2017-12-14 16:37:27 -0800

    pushing demo after lecture

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 const cors = require('cors');
+const superagent = require('superagent');
 
 app.use(express.static('./public'));
 app.use(cors());
+
+app.get('/github/*', (req, res) => {
+  // console.log(req.params[0]);
+  const url = `https://api.github.com/${req.params[0]}`
+
+  superagent.get(url)
+    .set(`Authorization`, `token ${process.env.GITHUB_TOKEN}`)
+    .then(
+      // successCallback, failureCallback
+      repos => res.send(repos.text),
+      err => res.send(err)
+    )
+
+})
 
 app.get('/', (req, res) => {
   res.sendFile('index.html')
